17b3c4e17c85e435409cf688b6fb8684527c091c
pupil <pupil@ivkhk.ee>
2020-09-07 15:10:56 +0300

    4 spaces to 2 spaces

--- src/calculator_kot/Calculator_Kot.java
+++ src/calculator_kot/Calculator_Kot.java
@@ -1,9 +1,8 @@
 package calculator_kot;
-    // Last version
+
 public class Calculator_Kot {
-    public static void main(String[] args) {
-        NewClass newClass = new NewClass();
-        newClass.run();
-    }
-    
+  public static void main(String[] args) {
+    NewClass newClass = new NewClass();
+    newClass.run();
+  }
 }
--- src/calculator_kot/NewClass.java
+++ src/calculator_kot/NewClass.java
@@ -11,53 +11,52 @@ import java.util.Scanner;
  * @author pupil
  */
 public class NewClass {
+
+  private Boolean play = true;
     
-    private Boolean play = true;
-    
-    public void run() {
-        System.out.println("Calculator");
-        do {
-            System.out.print("Enter first number: ");
-            Scanner scanner = new Scanner(System.in);
-            double number1 = scanner.nextDouble();
-            System.out.print("Enter second number: ");
-            double number2 = scanner.nextDouble();
-            scanner.nextLine();
-            System.out.print("Choose operation: (+ - * /): ");
-            String znak = scanner.nextLine();
+  public void run() {
+    System.out.println("Calculator");
+    do {
+      System.out.print("Enter first number: ");
+      Scanner scanner = new Scanner(System.in);
+      double number1 = scanner.nextDouble();
+      System.out.print("Enter second number: ");
+      double number2 = scanner.nextDouble();
+      scanner.nextLine();
+      System.out.print("Choose operation: (+ - * /): ");
+      String znak = scanner.nextLine();
             
-            switch (znak) {
-                case "+":
-                    System.out.print("Result: ");
-                    System.out.println(number1 + number2);
-                    break;
-                case "-":
-                    System.out.print("Result: ");
-                    System.out.println(number1 - number2);
-                    break;
-                case "*":
-                    System.out.print("Result: ");
-                    System.out.println(number1 * number2);
-                    break;
-                case "/":
-                    
-                    if (number1 == 0 || number2 == 0) {
-                        System.out.println("На ноль делить нельзя");
-                    } else {
-                        System.out.print("Result: ");
-                        System.out.println(number1 / number2);
-                    }
-                    break;
-                default:
-                    System.out.println("Not correct operation");
-                    break;
-            }
-            System.out.println("Exit? y / n");
-            String action = scanner.nextLine();
-            if (action.equals("y")) {
-                play = false;
-            }
-        } while (play);
-        System.out.println("Program finished");
-    }
+      switch (znak) {
+        case "+":
+          System.out.print("Result: ");
+          System.out.println(number1 + number2);
+          break;
+        case "-":
+          System.out.print("Result: ");
+          System.out.println(number1 - number2);
+          break;
+        case "*":
+          System.out.print("Result: ");
+          System.out.println(number1 * number2);
+          break;
+        case "/":
+          if (number1 == 0 || number2 == 0) {
+            System.out.println("На ноль делить нельзя");
+          } else {
+            System.out.print("Result: ");
+            System.out.println(number1 / number2);
+          }
+          break;
+        default:
+          System.out.println("Not correct operation");
+          break;
+      }
+      System.out.println("Exit? y / n");
+      String action = scanner.nextLine();
+      if (action.equals("y")) {
+        play = false;
+      }
+    } while (play);
+    System.out.println("Program finished");
+  }
 }
